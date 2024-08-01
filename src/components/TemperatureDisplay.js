e5d8ab0f111e0a7593c307f5c19a3e0b4adc36b5
h-willis <harry.willis@btconnect.com>
2024-08-01 13:46:37 +1200

    individual drop downs rather than all opening up, styling, fading in dropdowns

--- src/components/TemperatureDisplay.js
+++ src/components/TemperatureDisplay.js
@@ -1,17 +1,55 @@
 import React, { useState } from 'react';
 
-// const TEMPERATUREUNITS = {
-//   C: "Celsius",
-//   F: "Farenheit"
-// }
-
 const TEMPERATUREUNITS = {
   C: "°C",
   F: "°F"
 }
 
 const TemperatureDisplay = ({ temperatureData }) => {
+  const [isTempOpen, setIsTempOpen] = useState(false);
+  const [isHighOpen, setIsHighOpen] = useState(false);
+  const [isLowOpen, setIsLowOpen] = useState(false);
+  const [isFeelsOpen, setIsFeelsOpen] = useState(false);
+
+  function toggleIsOpen(e) {
+    switch (e.target.id) {
+      case 'temperature':
+        setIsTempOpen(!isTempOpen);
+        setIsHighOpen(false);
+        setIsLowOpen(false);
+        setIsFeelsOpen(false);
+        break;
+      case 'temperatureHigh':
+        setIsTempOpen(false);
+        setIsHighOpen(!isHighOpen);
+        setIsLowOpen(false);
+        setIsFeelsOpen(false);
+        break;
+      case 'temperatureLow':
+        setIsTempOpen(false);
+        setIsHighOpen(false);
+        setIsLowOpen(!isLowOpen);
+        setIsFeelsOpen(false);
+        break;
+      case 'temperatureFeel':
+        setIsTempOpen(false);
+        setIsHighOpen(false);
+        setIsLowOpen(false);
+        setIsFeelsOpen(!isFeelsOpen);
+        break;
+      default:
+        alert('WTF did you click?');
+    }
+  }
+
   const [units, setUnits] = useState(TEMPERATUREUNITS.C);
+  const handleOptionChange = ({ target }) => {
+    setUnits(target.value);
+    setIsTempOpen(false);
+    setIsHighOpen(false);
+    setIsLowOpen(false);
+    setIsFeelsOpen(false);
+  }
 
   function formatTemperature(temperature) {
     if (units === TEMPERATUREUNITS.C) {
@@ -22,25 +60,35 @@ const TemperatureDisplay = ({ temperatureData }) => {
     return farenheit.toFixed(2) + TEMPERATUREUNITS.F;
   }
 
-  return (
-    <div className="weatherContainer">
-      <span className='weatherItem'>
-        <img src='icons/thermometer.png' alt="Thermometer" /> {formatTemperature(temperatureData.temp)}
-      </span>
-      <span className='weatherItem'>
-        <img src='icons/thermometer_high.png' alt='Thermometer High' />{formatTemperature(temperatureData.temp_max)}
-      </span>
-      <span className='weatherItem'>
-        <img src='icons/thermometer_low.png' alt='Thermometer Low' />{formatTemperature(temperatureData.temp_min)}
-      </span>
-      <span className='weatherItem'>
-        <img src='icons/temperature-feels-like.svg' alt='Thermometer Feels Like' />{formatTemperature(temperatureData.feels_like)}
-      </span>
-      <select value={units} onChange={(e) => setUnits(e.target.value)}>
+  const optionDropdown = (
+    <div className={`tempUnitDropdown`}>
+      <select value={units} onClick={(e) => { e.stopPropagation(); }} onChange={handleOptionChange} >
         <option value={TEMPERATUREUNITS.C}>{TEMPERATUREUNITS.C}</option>
         <option value={TEMPERATUREUNITS.F}>{TEMPERATUREUNITS.F}</option>
-      </select>
-    </div>
+      </select >
+    </div>)
+
+  return (
+    <>
+      <div className="weatherContainer">
+        <span className='weatherItem' id='temperature' onClick={toggleIsOpen}>
+          <img src='icons/thermometer.png' alt="Thermometer" id='temperature' /> {formatTemperature(temperatureData.temp)}
+          {isTempOpen && optionDropdown}
+        </span>
+        <span className='weatherItem' id='temperatureHigh' onClick={toggleIsOpen}>
+          <img src='icons/thermometer_high.png' alt='Thermometer High' id='temperatureHigh' />{formatTemperature(temperatureData.temp_max)}
+          {isHighOpen && optionDropdown}
+        </span>
+        <span className='weatherItem' id='temperatureLow' onClick={toggleIsOpen}>
+          <img src='icons/thermometer_low.png' alt='Thermometer Low' id='temperatureLow' />{formatTemperature(temperatureData.temp_min)}
+          {isLowOpen && optionDropdown}
+        </span>
+        <span className='weatherItem' id='temperatureFeel' onClick={toggleIsOpen}>
+          <img src='icons/temperature-feels-like.svg' alt='Thermometer Feels Like' id='temperatureFeel' />{formatTemperature(temperatureData.feels_like)}
+          {isFeelsOpen && optionDropdown}
+        </span>
+      </div>
+    </>
   )
 }
 
