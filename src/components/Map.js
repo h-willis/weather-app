fa2b2599886c04dec0069c376cf63024c06c35d5
h-willis <harry.willis@btconnect.com>
2024-08-02 12:45:16 +1200

    fix broken default marker and clean up marker component remove popup as not used

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { useMap } from 'react-leaflet/hooks';
 import 'leaflet/dist/leaflet.css';
-// import L from 'leaflet';
+import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-// TODO fix the default marker not showing
-// // Fix the default marker icon issue
-// delete L.Icon.Default.prototype._getIconUrl;
+// Set up the default icon for markers
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
 
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-//   iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-//   shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-// });
+L.Marker.prototype.options.icon = DefaultIcon;
 
-// Leaflet maps are immutable so creating another with TileLayer doesn't work,
+
+// Leaflet maps are immutable so just updating position doesn't work to cause
+// tile layer to update
 // This sits as a child of the MapContainer 
 function ChangeView({ position }) {
   const map = useMap();
@@ -28,16 +30,12 @@ function Map({ coords }) {
 
   return (
     <div className="map">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%", borderRadius: "6px" }}>
+      <MapContainer center={position} zoom={50} scrollWheelZoom={false} style={{ height: "100%", width: "100%", borderRadius: "6px" }}>
         <ChangeView position={position} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={position} />
       </MapContainer>
     </div>
   );
