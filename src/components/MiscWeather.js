46cf4f93d818241dd2e10d6c0d2fa86d98099711
h-willis <80955964+h-willis@users.noreply.github.com>
2024-08-02 14:45:02 +1200

    7 add options for units for remaining weather items (#9)
    * add weather description and styling
    
    * dropdown and selection for options of wind units, need to implement value formulas
    
    * first pass speed conversion maths
    
    * refactor wind units conversion fnc, tweak layout to stop overflow of units
    
    * add clickable class to css and weather items that have unit options

--- src/components/MiscWeather.js
+++ src/components/MiscWeather.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // humidity, pressure, cloud coverage, wind speed
+const WINDUNITS = {
+  MS: "m/s",
+  FTS: "ft/s",
+  MPH: "mph",
+  KNOTS: "knots",
+  KMPH: "km/h"
+}
 
 function MiscWeather({ data }) {
+  const [windUnits, setWindUnits] = useState(WINDUNITS.MS);
+  function handleOptionChange({ target }) {
+    setWindUnits(target.value);
+    setIsOpen(false);
+  }
+
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleIsOpen() {
+    setIsOpen(!isOpen);
+  }
+
+  function formatWindSpeed(speedMs) {
+    let convertedSpeed = speedMs;
+    switch (windUnits) {
+      case WINDUNITS.FTS:
+        convertedSpeed = speedMs * 3.28084;
+        break;
+      case WINDUNITS.MPH:
+        convertedSpeed = speedMs * 2.23694;
+        break;
+      case WINDUNITS.KNOTS:
+        convertedSpeed = speedMs * 1.94384;
+        break;
+      case WINDUNITS.KMPH:
+        convertedSpeed = speedMs * 3.6;
+        break;
+      default:
+        console.log(`Unknown units: ${windUnits}`);
+        break;
+    }
+
+    return convertedSpeed.toFixed(2) + windUnits;
+  }
+
+  const windOptionDropdown = (
+    <div className={`unitsDropdown`}>
+      <select value={windUnits} onClick={(e) => { e.stopPropagation(); }} onChange={handleOptionChange} >
+        <option value={WINDUNITS.MS}>{WINDUNITS.MS}</option>
+        <option value={WINDUNITS.FTS}>{WINDUNITS.FTS}</option>
+        <option value={WINDUNITS.MPH}>{WINDUNITS.MPH}</option>
+        <option value={WINDUNITS.KNOTS}>{WINDUNITS.KNOTS}</option>
+        <option value={WINDUNITS.KMPH}>{WINDUNITS.KMPH}</option>
+      </select >
+    </div>);
+
   return (
     <div className="weatherContainer">
-      <span className="weatherItem" onClick={() => alert("you clicked me")}>
+      <span className="weatherItem">
         <img src='icons/humidity.png' alt="Himidity" /> {data.main.humidity}%
       </span>
       <span className="weatherItem">
@@ -14,8 +66,9 @@ function MiscWeather({ data }) {
       <span className="weatherItem">
         <img src='icons/cloud.png' alt='Cloud' />{data.clouds.all}%
       </span>
-      <span className="weatherItem">
-        <img src='icons/wind.png' alt='Wind' />{data.wind.speed}m/s
+      <span className="weatherItem clickable" onClick={toggleIsOpen}>
+        <img src='icons/wind.png' alt='Wind' />{formatWindSpeed(data.wind.speed)}
+        {isOpen && windOptionDropdown}
       </span>
     </div>
   );
--- src/components/TemperatureDisplay.js
+++ src/components/TemperatureDisplay.js
@@ -61,7 +61,7 @@ const TemperatureDisplay = ({ temperatureData }) => {
   }
 
   const optionDropdown = (
-    <div className={`tempUnitDropdown`}>
+    <div className={`unitsDropdown`}>
       <select value={units} onClick={(e) => { e.stopPropagation(); }} onChange={handleOptionChange} >
         <option value={TEMPERATUREUNITS.C}>{TEMPERATUREUNITS.C}</option>
         <option value={TEMPERATUREUNITS.F}>{TEMPERATUREUNITS.F}</option>
@@ -71,19 +71,19 @@ const TemperatureDisplay = ({ temperatureData }) => {
   return (
     <>
       <div className="weatherContainer">
-        <span className='weatherItem' id='temperature' onClick={toggleIsOpen}>
+        <span className='weatherItem clickable' id='temperature' onClick={toggleIsOpen}>
           <img src='icons/thermometer.png' alt="Thermometer" id='temperature' /> {formatTemperature(temperatureData.temp)}
           {isTempOpen && optionDropdown}
         </span>
-        <span className='weatherItem' id='temperatureHigh' onClick={toggleIsOpen}>
+        <span className='weatherItem clickable' id='temperatureHigh' onClick={toggleIsOpen}>
           <img src='icons/thermometer_high.png' alt='Thermometer High' id='temperatureHigh' />{formatTemperature(temperatureData.temp_max)}
           {isHighOpen && optionDropdown}
         </span>
-        <span className='weatherItem' id='temperatureLow' onClick={toggleIsOpen}>
+        <span className='weatherItem clickable' id='temperatureLow' onClick={toggleIsOpen}>
           <img src='icons/thermometer_low.png' alt='Thermometer Low' id='temperatureLow' />{formatTemperature(temperatureData.temp_min)}
           {isLowOpen && optionDropdown}
         </span>
-        <span className='weatherItem' id='temperatureFeel' onClick={toggleIsOpen}>
+        <span className='weatherItem clickable' id='temperatureFeel' onClick={toggleIsOpen}>
           <img src='icons/temperature-feels-like.svg' alt='Thermometer Feels Like' id='temperatureFeel' />{formatTemperature(temperatureData.feels_like)}
           {isFeelsOpen && optionDropdown}
         </span>
